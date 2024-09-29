0276dfb5c1d46e4a90e13d59fed4711db0ea63ab
Satyam-Nayak <satyamn120@gmail.com>
2024-09-29 09:17:45 +0530

    newww

---
+++ Modules/local modules try/Destructuring Import/main.js
@@ -0,0 +1,5 @@
+const { add, sub, multiply } = require('./module_exports');
+
+console.log(add(10, 20));      // Output: 30
+console.log(sub(30, 10));      // Output: 20
+console.log(multiply(10, 5));  // Output: 50
