20ba6e7245b33f473108db14e163784d449616f5
Satyam-Nayak <satyamn120@gmail.com>
2024-09-29 09:16:22 +0530

    29=09=2024

---
+++ Modules/local modules try/Single Import (Using local Object)/main.js
@@ -0,0 +1,5 @@
+const local = require('./module_exports');
+
+console.log(local.add(10, 20));      // Output: 30
+console.log(local.sub(30, 10));      // Output: 20
+console.log(local.multiply(10, 5));  // Output: 50
---
+++ Modules/local modules try/Single Import (Using local Object)/module_exports.js
@@ -0,0 +1,13 @@
+let add = (a, b) => {
+    return a + b;
+}
+
+let sub = (a, b) => {
+    return a - b;
+}
+
+let multiply = (a, b) => {
+    return a * b;
+}
+
+module.exports = { add, sub, multiply };
