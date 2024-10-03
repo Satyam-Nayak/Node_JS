919ddb799b036d3139cb6f695c85244c0f40750a
Satyam-Nayak <satyamn120@gmail.com>
2024-10-03 15:41:18 +0530

    new 03-10-2024

---
+++ 03-10-2024 node js class/FS_Async.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+// console.log(fs)
+
+
+//Sync way
+// let data1=fs.readFileSync('./note.txt','utf-8')
+// console.log(data1)
+
+
+
+//! Read a file aysnc manner
+// let data=fs.readFile('./note.txt','utf-8',(err,data)=>{
+//     if(err)
+//         throw err;
+//      console.log(data);
+// })
+
+
+//?create file inside nested folder
+// fs.writeFileSync('./node.txt',"node")
+// console.log("file created")
+
+//write  a file using async 
+// let data1 = fs.writeFile('./no.txt', 'node', (err,data1) => {
+//     if(err)
+//         throw err;
+//     console.log(data1)
+//      console.log("file created ");
+// })
+
+
+// append a file 
+// delete a file 
+// rename a file
