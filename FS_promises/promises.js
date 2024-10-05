2aa15834f69a79230eb115ca613d957d40783802
Satyam-Nayak <satyamn120@gmail.com>
2024-10-05 13:10:24 +0530

    task yesterday node js

--- FS_promises/promises.js
+++ FS_promises/promises.js
@@ -1,6 +1,5 @@
 const { create } = require('domain')
 const fs = require('fs/promises')
-const { fileFromPath } = require('openai/_shims/fileFromPath.node')
 
 // fs.readFile('./promises.txt','utf-8')
 // .then((data)=>{console.log(data)})
@@ -12,12 +11,101 @@ const { fileFromPath } = require('openai/_shims/fileFromPath.node')
 
 // ======task===
 // read n write file
+// fs.readFile('./promises.txt', 'utf-8')
+//   .then((data) => {
+//     console.log(data);  // Log the content of the file
+//   })
+//   .catch((err) => {
+//     console.log(err);  // Log any errors if the file read fails
+//   });
+
 // rename a file
+// fs.rename('./note.txt', 'Satyam.txt')
+// .then((data)=>{
+//     console.log("file rename susscesful");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+
+
 // delete a file
+// fs.unlink('./s1.txt')
+// .then((_)=>{
+//     console.log("s1.txt file deteled");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+
 // change file extension
+// fs.rename('./sam.txt','./sam.js')
+// .then((_)=>{
+//     console.log("sam.txt file rename to sam.js file");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
 
 // create a Folder 
+// fs.mkdir('./Satyam')
+// .then((_)=>{
+//     console.log("Folder created");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+
 // create a nested Folder
+// fs.mkdir('./Satyam/Sonu')
+// .then((_)=>{
+//     console.log("nested folder created");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
 // create file inside a Folder
-// rename folder 
-// delete folder 
+// fs.writeFile('./Satyam/Sonu/sam.txt', 'Hii am file inside the sonu folder which also inside the satyam folder')
+// .then((_)=>{
+//     console.log("file created");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+// rename folder
+// fs.rename('./Satyam/Sonu/sam.txt','./Satyam/Sonu/som.txt')
+// .then((_)=>{
+//     console.log("sam.txt file rename to som.txt file");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+
+// delete folder
+fs.unlink('./Satyam26/Sonu/som.txt')
+.then((_)=>{
+    console.log("som.txt file deleted");
+    fs.rmdir('./Satyam26/Sonu')
+    .then((_)=>{
+        console.log("folder deleted");
+        fs.rmdir('./Satyam26')
+        .then((_)=>{
+            console.log("Satyam folder deleted");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+.catch((err)=>{
+    console.log(err);
+})
+
