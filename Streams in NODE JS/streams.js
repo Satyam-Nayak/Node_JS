232e0ae220124dd3dd276fd7a6e4411879690b15
Satyam-Nayak <satyamn120@gmail.com>
2024-10-07 22:05:58 +0530

    Streams Node JS 3

--- Streams in NODE JS/streams.js
+++ Streams in NODE JS/streams.js
@@ -1,20 +1,22 @@
-//?Streams in Node JS ?
+//?Streams in Node JS 
+// Streams:
+// Streams are collection of data like arrays or strings
+// Streams might not available all at once
+// Streams don't have to fit in memory
+// Used to handle or manipulate the data like videos, larger files etc. ....
+// Streams are used to transfer the data chunk by chunk...
 const fs = require('fs')
 
-
-
-
-
-
-
-//! 1. Readable Stream: 
-//! 2. Writeable Stream: These are the streams which are used to write the data to the files and network i/o connections
-//! 3. Duplex Stream: These are the streams which are used to read and write the 
 
 // Types of Steram
 // 1. Readable Stream?
 // 2. Writable Stream
 // 3. Duplex Stream
+
+//! 1. Readable Stream: These streams are used to read data from files or other sources in chunks, without loading everything into memory at once.
+//! 2. Writeable Stream: These are the streams which are used to write the data to the files and network i/o connections
+//! 3. Duplex Stream: These are the streams which are used to read and write the data at a time
+
 
 
 
@@ -37,11 +39,7 @@ const fs = require('fs')
 
 
 
-//!Events:
-// data 
-// end 
-// error
-// close
+
 
 // readData.on('end',_=>{
 //     console.log("Reading is finished")
