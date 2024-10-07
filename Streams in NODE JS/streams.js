6af1b6a5ef09d42c4df451f8d6f93c8deca8a057
Satyam-Nayak <satyamn120@gmail.com>
2024-10-07 15:42:24 +0530

    Streams Node JS 1

--- Streams in NODE JS/streams.js
+++ Streams in NODE JS/streams.js
@@ -16,15 +16,23 @@
 
 
 
-
-const { error } = require('console')
+//////////===================================================
 const fs = require('fs')
-let readData= fs.createReadStream('./streams1.js')
+let readData= fs.createReadStream('./streams1.js','utf-8')
 readData.on('data',data=>{
     console.log(data)
 })
 
 // console.log(readData)
+
+//if we donot use utf-8 {( "let readData= fs.createReadStream('./streams1.js')" )}
+//When reading from a file without specifying the encoding (like `utf-8`), 
+//Node.js treats the data as raw binary, which is represented as a `Buffer` object. 
+//Buffers store binary data and are not automatically 
+//converted to strings unless an encoding like `utf-8` is provided to interpret the data as text.
+//////////=================End=================
+
+
 
 //!Events:
 // data 
