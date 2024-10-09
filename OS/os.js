2736100d7d99cf9d147a7c621017b5ab0db2d0cf
Satyam-Nayak <satyamn120@gmail.com>
2024-10-09 15:49:42 +0530

    os use in node js

--- OS/os.js
+++ OS/os.js
@@ -1,9 +1,10 @@
 let os = require('os')
 console.log(os.platform())
 console.log(os.arch())
-console.log(os.hostname())
+console.log(os.hostname()) // host name of the system
 console.log(os.userInfo())
 console.log(os.freemem()/1024/1024/1024)  // free memory present in the system
 console.log(os.totalmem()/1024/1024/1024)
 console.log(os.tmpdir())
 console.log(os.type())
+console.log(os.machine())
