451e79f5740928f24f1be2669fb024dd353dbfb9
Satyam-Nayak <satyamn120@gmail.com>
2024-10-09 15:37:18 +0530

    Url use in Node JS

---
+++ URL/url.js
@@ -0,0 +1,12 @@
+// let url1 = "https://www.flipkart.com/clothing-and-accessories/bottomwear/cargos/mens-cargos/pr?sid=clo%2Cvua%2Crqy%2Cnli"
+// let url3= "https://www.instagram.com/explore/"
+// let url2 = "https://localhost:3001/about?userid=1&age=22"
+
+// const url = require('url')
+// let urldata = url.parse(url1,true)
+// console.log(urldata.hostname)
+// console.log(urldata.port)
+// console.log(urldata.query)
+// console.log(urldata.query.userid)
+
+
